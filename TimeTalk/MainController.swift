cb96e52e0a25b52b0b635ad4f450866cbc685f25
Feihong Hsu <feihong.hsu@gmail.com>
2016-01-28 14:30:54 -0600

    Add project files

---
+++ TimeTalk/MainController.swift
@@ -0,0 +1,70 @@
+import UIKit
+import AVFoundation
+
+
+private let calendar = NSCalendar.currentCalendar()
+private var synthesizer = AVSpeechSynthesizer()
+private var voice = AVSpeechSynthesisVoice(language: "en-US")
+
+
+class MainController: UIViewController {
+    @IBOutlet var timeLabel: UILabel!
+    var started = false
+    var timer: CancelableTimer!
+    var previousMinute = -1
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateTimeLabel()
+        
+        timer = CancelableTimer(once: false, handler: updateTimeLabel)
+        timer.startWithInterval(1.0)
+    }
+    
+    @IBAction func startOrStop(sender: UIButton) {
+        started = !started
+        sender.setTitle(started ? "Stop" : "Start", forState: .Normal)
+        if started {
+            let (hour, minute) = getHourAndMinute()
+            speakHour(hour, andMinute: minute)
+        }
+    }
+    
+    func updateTimeLabel() {
+        let (hour, minute) = getHourAndMinute()
+        if previousMinute != minute {
+            previousMinute = minute
+            timeLabel.text = String(format: "%02d:%02d", hour, minute)
+            
+            if started {
+                speakHour(hour, andMinute: minute)
+            }
+        }
+    }
+}
+
+
+func speak(text: String) {
+    let utterance = AVSpeechUtterance(string: text)
+    utterance.voice = voice
+    // Interrupt any phrase that's currently being spoken.
+    if synthesizer.speaking {
+        synthesizer.stopSpeakingAtBoundary(.Immediate)
+    }
+    synthesizer.speakUtterance(utterance)
+}
+
+func speakHour(hour: Int, andMinute minute: Int) {
+    let text = (minute < 10) ?
+        "\(hour) O \(minute)" : "\(hour) \(minute)"
+    speak(text)
+}
+
+func getHourAndMinute() -> (Int, Int) {
+    let now = NSDate()
+    let comp = calendar.components([.Hour, .Minute], fromDate: now)
+    let hour = comp.hour % 12
+    let minute = comp.minute
+    return (hour, minute)
+}
